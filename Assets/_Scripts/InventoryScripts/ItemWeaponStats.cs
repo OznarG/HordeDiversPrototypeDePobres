75080b2419d638c122236a59caff368dd89e6051
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-05-25 19:01:00 -0400

    Created Weapon Script
    Need to add it to the slot switch, add, or remove methods.

---
+++ Assets/_Scripts/InventoryScripts/ItemWeaponStats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ItemWeaponStats : MonoBehaviour
+{
+    [SerializeField] int weaponLevel;
+    [SerializeField] float damage;
+    [SerializeField] float strength;
+    [SerializeField] float speed;
+    [SerializeField] string _name;
+}
