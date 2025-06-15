5a57c74fc331cabc6543833b9ff30dba90bb744d
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-06-15 08:20:16 -0400

    Fixed bug in farmeable tree

--- Assets/_Scripts/ObjectsBehaviours/FarmeableItem.cs
+++ Assets/_Scripts/ObjectsBehaviours/FarmeableItem.cs
@@ -8,17 +8,22 @@ public class FarmeableItem : MonoBehaviour
     public ItemType damagableBy;
     [SerializeField] int indexDamageTool;
     [SerializeField] int dropAmount;
+    bool dead;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void TakeDamage(float _damage, int index)
     {
         if(indexDamageTool == index)
         {
-            AudioManager.instance.PlaySFX(clip);
-            health -= _damage;
-            if(health <= 0 )
+            if(!dead)
             {
-                itemToDrop.amountToAdd = 5;
+                AudioManager.instance.PlaySFX(clip);
+            }
+
+            health -= _damage;
+            if(health <= 0 && !dead)
+            {
+                dead = true;               
                 gameManager.instance.playerInventoryScript.AddItem(itemToDrop, itemToDrop.amountToAdd + dropAmount);
                 Destroy(gameObject, 5);
             }
